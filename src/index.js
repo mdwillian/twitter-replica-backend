f7f173d947012e1d62a171dbccb4ddf1238325d4
Willian Domingos <mdwillian@gmail.com>
2019-05-28 01:05:25 -0300

    Adicionar dados de configuração do database em .env

--- src/index.js
+++ src/index.js
@@ -7,23 +7,29 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://will:147963@cluster0-rmwqy.mongodb.net/goweek', {
+require('dotenv').config();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${
+    process.env.DB_HOST
+  }/${process.env.DB_DATABASE}`,
+  {
     useNewUrlParser: true
-});
+  }
+);
 
 app.use(cors());
 
 //Middware - Deixando o io global para a aplicação
 app.use((req, res, next) => {
-    req.io = io;
+  req.io = io;
 
-    return next();
+  return next();
 });
 
 app.use(express.json());
 app.use(require('./routes'));
 
 server.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server running on port 3000');
 });
-
